f7160df961a1b9e94b07e86e9fb0406cc4a613d6
[VARADHARAJAN4] <85033135+VARADHARAJAN4@users.noreply.github.com>
2023-03-03 09:56:52 +0530

    sdfg

--- Cglearners/src/Activity/Friday0903953.java
+++ Cglearners/src/Activity/Friday0903953.java
@@ -4,7 +4,7 @@ public class Friday0903953 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Today: Nikita Kadam");
+		System.out.println("Tdfhgljkdhoday: Nikita Kadam");
 	}
 
 }
